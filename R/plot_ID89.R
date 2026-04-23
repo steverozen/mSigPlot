fd906eab37f0fc78206eb5dba51393f37bcdeefd
Steven G. Rozen <steverozen@pm.me>
2026-04-23 16:27:55 -0400

    standardized in "open" rather than stop at R = 9; ploting is the same; user has to know where the data were trimmed

--- R/plot_ID89.R
+++ R/plot_ID89.R
@@ -12,15 +12,11 @@ id89_figlabels <- function(indel_types) {
   x[is_single] <- gsub("(Del|Ins)\\(([CT])\\)", "\\2", x[is_single])
   x[is_single] <- gsub(":R", "", x[is_single])
   x[is_single] <- gsub("\\(([0-9]+),\\)", "(\\1,9)", x[is_single])
-  x[is_single] <- gsub("[][]", "|", x[is_single])
-  x[is_single] <- sub("^\\|", "", x[is_single])
-  x[is_single] <- gsub("\\)\\|", ")", x[is_single])
 
   x[is_multi] <- sub("^(Del|Ins)\\(", "L(", x[is_multi])
   x[is_multi] <- sub("^L\\(([0-9]+),\\)", "L(\\1, )", x[is_multi])
   x[is_multi] <- sub("R\\(([0-9]+),\\)$", "R(\\1,9)", x[is_multi])
   x[is_multi] <- sub("M\\(([0-9]+),\\)$", "M(\\1, )", x[is_multi])
-  x[is_multi] <- gsub(":", "", x[is_multi])
 
   x
 }
